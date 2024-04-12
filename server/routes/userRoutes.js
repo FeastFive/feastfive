bb6c9de83a21982f830c7345e75c3c370f7665bd
Zehra Uğurlu <zeugurlu@outlook.com>
2024-04-12 20:27:15 +0300

    Update on controller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup } = require("../controllers/authController");
-router.post("/signup", signup);
+const { registerUser } = require("../controllers/authController");
+router.post("/", registerUser);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,14 +4,13 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const http = require("http");
-const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 var cors = require("cors");
+
 require("dotenv").config();
 
 const port = process.env.PORT || 4000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
@@ -20,13 +19,18 @@ app.use(
   })
 );
 
+app.use(express.static(path.resolve(__dirname, "../build")));
+
+const url = process.env.DATABASE;
+const dbName = process.env.DBNAME;
+const server = http.createServer(app);
+
 connectDB();
 
 //Routes
-
 //for user
 app.use("/api/users", require("./routes/userRoutes"));
 
-const server = app.listen(port, () => {
+server.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
