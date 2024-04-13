28f3e5103acdf18a592e85efddc88dd4f04ae642
Zehra Uğurlu <zeugurlu@outlook.com>
2024-04-13 14:47:23 +0300

    changes

--- server/server.js
+++ server/server.js
@@ -1,35 +1,29 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const path = require("path");
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require("path");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-var cors = require("cors");
 
-require("dotenv").config();
+dotenv.config();
 
 const port = process.env.PORT || 4000;
 
+//Middlewares
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
 app.use(express.static(path.resolve(__dirname, "../build")));
 
-const url = process.env.DATABASE;
-const dbName = process.env.DBNAME;
-const server = http.createServer(app);
-
+//DB connection
 connectDB();
 
 //Routes
-//for user
 app.use("/api/users", require("./routes/userRoutes"));
+
+//Server
+const server = http.createServer(app);
 
 server.listen(port, () => {
   console.log(`App running on port ${port}...`);
