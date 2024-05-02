685d112dc882709527a4fff9cd48e5815bb7be17
Zehra Uğurlu <zeugurlu@outlook.com>
2024-05-02 19:56:43 +0300

    import update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import 'swiper/css';
+import "swiper/css";
 
 let persistor = persistStore(store);
 
