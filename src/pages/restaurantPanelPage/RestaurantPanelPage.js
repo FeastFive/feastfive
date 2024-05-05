3c0ce1781c6572d687e9081aebad6741ef67638a
Eren Kaya <19soft1096@isik.edu.tr>
2024-05-05 14:43:29 +0300

    dynamicLabelingHasDone

--- src/pages/restaurantPanelPage/RestaurantPanelPage.js
+++ src/pages/restaurantPanelPage/RestaurantPanelPage.js
@@ -7,27 +7,44 @@ import RestaurantPanelMenu from "../../components/RestaurantPanelMenu";
 import { useNavigate } from "react-router-dom";
 import Select from "react-select";
 import makeAnimated from "react-select/animated";
-
+import { getKitchen } from "../../utils/kitchen/getKitchen";
+import { useEffect } from "react";
 const RestaurantPanelPage = () => {
   const restaurantData = useSelector((state) => state.restaurant);
   console.log(restaurantData.meals);
   const navigate = useNavigate();
   const animatedComponents = makeAnimated();
-  const options = [
-    { value: "Pasta", label: "Pasta" },
-    { value: "Meat", label: "Meat" },
-    { value: "Chicken", label: "Chicken" },
-    { value: "Pizza", label: "Pizza" },
-    { value: "Kebap", label: "Kebap" },
-    { value: "Salad", label: "Salad" },
-    { value: "Burger", label: "Burger" },
-    { value: "Sushi", label: "Sushi" },
-    { value: "Homemade", label: "Homemade" },
-    { value: "Vegan", label: "Vegan" },
-    { value: "Desert", label: "Desert" },
-    { value: "Sandwich", label: "Sandwich" },
-    { value: "Drinks", label: "Drinks" },
-  ];
+
+  const [categories, setCategories] = useState();
+  useEffect(() => {
+    const handleKitchen = async () => {
+      try {
+        const response = await getKitchen();
+
+        if (response.status === 200) {
+          const result = await response.json();
+          // console.log(result.kitchens[0].image);
+          setCategories(result.kitchens);
+        } else if (response.status === 403) {
+          ShowAlert(3, "An error occurred while fetching kitchens");
+        } else {
+          ShowAlert(3, "An error occurred while fetching kitchens");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        ShowAlert(3, "An error occurred while fetching kitchens");
+      }
+    };
+    handleKitchen();
+  }, []);
+  console.log(categories);
+  const options = categories
+    ? categories.map((category) => ({
+        value: category.name,
+        label: category.name,
+      }))
+    : [];
+
   const dispatch = useDispatch();
 
   // State to store selected options
