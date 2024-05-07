f20b857835dc92b031d52dc2a7b03505d12357da
Eren Kaya <19soft1096@isik.edu.tr>
2024-05-07 18:07:17 +0300

    start

---
+++ src/pages/profilePage/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Profile.module.css";
+import Navbar from "../../components/Navbar";
+
+const Profile = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Profile;
