dfc0c6cab9e9617ddfb16fb8d92d87ba537c45f5
Zehra Uğurlu <zeugurlu@outlook.com>
2024-05-13 14:03:35 +0300

    navigation for search

--- src/components/search/SearchResult.js
+++ src/components/search/SearchResult.js
@@ -1,8 +1,12 @@
-export const SearchResult = ({ result }) => {
+import { useNavigate } from "react-router-dom";
+
+export const SearchResult = ({ result, resultId }) => {
+  const navigate = useNavigate();
   return (
     <div
       className="search-result"
-      onClick={(e) => alert(`You selected ${result}!`)}
+      // onClick={(e) => alert(`You selected ${result}!`)}
+      onClick={() => navigate(`/restaurantFoods/${resultId}`)}
     >
       {result}
     </div>
--- src/components/search/SearchResultList.js
+++ src/components/search/SearchResultList.js
@@ -5,7 +5,13 @@ function SearchResultList({ results }) {
   return (
     <div className="flex bg-[#F9FCFB] text-left cursor-pointer flex-col border-2 border-t-0 rounded-b-lg px-4 text-lg gap-2 h-auto py-2 shadow-md shadow-gray-100 w-full">
       {results.map((result, id) => {
-        return <SearchResult result={result.restaurantName} key={id} />;
+        return (
+          <SearchResult
+            result={result.restaurantName}
+            resultId={result._id}
+            key={id}
+          />
+        );
       })}
     </div>
   );
